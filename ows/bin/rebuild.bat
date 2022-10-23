25d463f3545a35003378928b29ce6c33c0fb4008
Saydum Halibekov <saydum.it5@gmail.com>
2022-10-23 11:32:25 +0300

    ...

---
+++ ows/bin/rebuild.bat
@@ -0,0 +1,44 @@
+cd ../
+
+Set FILE=.env
+
+For /F "PREFIX=* delims=" %%i In ("%FILE%") Do Set PREFIX=%%i
+For /F "PHP_VERSION=* delims=" %%i In ("%FILE%") Do Set PHP_VERSION=%%i
+For /F "NODE_VERSION=* delims=" %%i In ("%FILE%") Do Set NODE_VERSION=%%i
+For /F "MONGODB_VERSION=* delims=" %%i In ("%FILE%") Do Set MONGODB_VERSION=%%i
+
+docker stop %PREFIX%webserver
+docker rm %PREFIX%webserver
+
+docker stop %PREFIX%php
+docker rm %PREFIX%php
+docker rmi openweb/php:%PHP_VERSION%
+
+docker stop %PREFIX%node
+docker rm %PREFIX%node:%NODE_VERSION%
+
+docker stop %PREFIX%mysql
+docker rm %PREFIX%mysql
+
+docker stop %PREFIX%pgsql
+docker rm %PREFIX%pgsql
+
+docker stop %PREFIX%redis
+docker rm %PREFIX%redis
+
+docker stop %PREFIX%rabbitmq
+docker rm %PREFIX%rabbitmq
+
+docker stop %PREFIX%memcached
+docker rm %PREFIX%memcached
+
+docker stop %PREFIX%mongo
+docker rm %PREFIX%mongo:%MONGODB_VERSION%
+
+docker stop %PREFIX%clickhouse-server
+docker rm %PREFIX%clickhouse-server
+
+docker stop %PREFIX%clickhouse-client
+docker rm %PREFIX%clickhouse-client
+
+pause
